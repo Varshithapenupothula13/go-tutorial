42faa1c3c593d142691cf653b2dd88cf8cba2329
Varshitha Penupothula <Penupothulavarshitha@gmail.com>
2026-07-24 14:54:10 +0530

    Task #1810 Explored arrays and element access in Go

---
+++ arrays.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+    var numbers = [5]int{10, 20, 30, 40, 50}
+
+    fmt.Println("Array:", numbers)
+    fmt.Println("First element:", numbers[0])
+    fmt.Println("Third element:", numbers[2])
+}
