9c837116fa46dbd55de27d7167386603b0d16f56
Varshitha Penupothula <Penupothulavarshitha@gmail.com>
2026-07-24 14:04:41 +0530

    Task #1810 Learned short variable declaration in Go

---
+++ short_variables.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+    name := "Varshitha"
+    age := 22
+    learningGo := true
+
+    fmt.Println("Name:", name)
+    fmt.Println("Age:", age)
+    fmt.Println("Learning Go:", learningGo)
+}
