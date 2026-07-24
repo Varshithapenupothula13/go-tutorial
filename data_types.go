c40aeb2d83827c04d63e919f4c38271fa179b5ce
Varshitha Penupothula <Penupothulavarshitha@gmail.com>
2026-07-24 14:10:06 +0530

    Task #1810 Learned basic data types in Go

---
+++ data_types.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+    var age int = 22
+    var height float64 = 5.4
+    var name string = "Varshitha"
+    var learningGo bool = true
+
+    fmt.Println("Age:", age)
+    fmt.Println("Height:", height)
+    fmt.Println("Name:", name)
+    fmt.Println("Learning Go:", learningGo)
+}
