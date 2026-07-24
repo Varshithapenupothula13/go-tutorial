63edd6ee42fdfcbd7d0cd9477f3086b60443cd60
Varshitha Penupothula <Penupothulavarshitha@gmail.com>
2026-07-24 19:01:29 +0530

    Task #1810 Integrated database package with application structure

---
+++ database/database.go
@@ -0,0 +1,7 @@
+package database
+
+import "fmt"
+
+func ConnectDatabase() {
+	fmt.Println("Database connection initialized")
+}
--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 
+	"go_tutorial/database"
 	"go_tutorial/routes"
 )
 
 func main() {
 	fmt.Println("Go Tutorial Application Started")
 
+	database.ConnectDatabase()
 	routes.SetupRoutes()
 }
